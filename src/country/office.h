cfc6981b026358236d9177f65d067fac094a6f16
Andrettin <6322423+Andrettin@users.noreply.github.com>
2024-12-28 01:00:06 +0100

    Offices can now have a civilian unit class assigned to them

--- src/country/office.h
+++ src/country/office.h
@@ -3,9 +3,12 @@
 #include "database/data_type.h"
 #include "database/named_data_entry.h"
 
+Q_MOC_INCLUDE("unit/civilian_unit_class.h")
+
 namespace kobold {
 
 class character_attribute;
+class civilian_unit_class;
 class country_attribute;
 
 template <typename scope_type>
@@ -16,6 +19,7 @@ class office final : public named_data_entry, public data_type<office>
 	Q_OBJECT
 
 	Q_PROPERTY(int attribute_modifier_divisor MEMBER attribute_modifier_divisor READ get_attribute_modifier_divisor NOTIFY changed)
+	Q_PROPERTY(const kobold::civilian_unit_class* civilian_unit_class MEMBER civilian_unit_class READ get_civilian_unit_class NOTIFY changed)
 
 public:
 	static constexpr const char class_identifier[] = "office";
@@ -44,6 +48,11 @@ public:
 		return this->attribute_modifier_divisor;
 	}
 
+	const kobold::civilian_unit_class *get_civilian_unit_class() const
+	{
+		return this->civilian_unit_class;
+	}
+
 	const and_condition<character> *get_conditions() const
 	{
 		return this->conditions.get();
@@ -56,6 +65,7 @@ private:
 	std::vector<const character_attribute *> character_attributes;
 	std::vector<const country_attribute *> country_attributes;
 	int attribute_modifier_divisor = 0;
+	const kobold::civilian_unit_class *civilian_unit_class = nullptr;
 	std::unique_ptr<const and_condition<character>> conditions;
 };
 
